61a313ff8801f6e7e15d7913266606087d5dee16
gustavo <gustavo.gigante.s@gmail.com>
2023-03-25 12:16:23 +0000

    FontAwesome, SweetAlert, UI/UX improvements

--- frontend/src/pages/TransactionsOverviewPage/TransactionsForm.js
+++ frontend/src/pages/TransactionsOverviewPage/TransactionsForm.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { useForm, Controller } from "react-hook-form";
 import { DatePicker } from 'antd';
+import Swal from 'sweetalert2';
 import useAxios from 'utils/axios.interceptors';
 import './transactionsForm.scss';
 
-const TransactionsForm = ({ isOpen = false, setIsOpen = () => {} }) => {
+const TransactionsForm = ({ isOpen = false, setIsOpen = () => {}, loadTransactions }) => {
   const api = useAxios();
-  const { register, handleSubmit, control, setValue, formState: { errors } } = useForm();
+  const { register, handleSubmit, control, setValue, reset, formState: { errors } } = useForm();
 
   if (!isOpen) return null;
 
   const onSubmit = async (data) => {
-    const transaction = await api.post('/api/user_expenses', data);
-    console.log(transaction)
+    try {
+    await api.post('/api/user_expenses', data);
+    Swal.fire({
+      icon: 'success',
+      title: 'Transaction created',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    await loadTransactions();
+    setIsOpen(false);
+    reset();
+    } catch(e) {
+      console.log(e)
+      Swal.fire({
+        title: 'Failed to create transaction',
+        text: e.message,
+        icon: 'error',
+        confirmButtonText: 'Close',
+      })
+    }
   }
 
   return (
--- frontend/src/pages/TransactionsOverviewPage/TransactionsOverviewPage.js
+++ frontend/src/pages/TransactionsOverviewPage/TransactionsOverviewPage.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useContext } from 'react';
 import { format } from 'date-fns';
+import Swal from 'sweetalert2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import useAxios from 'utils/axios.interceptors';
 import TransactionsForm from './TransactionsForm';
 import ArrowRight from '../../assets/arrow-right.svg';
@@ -9,30 +12,35 @@ import './transactionsOverview.scss';
 const TransactionsOverviewPage = () => {
   const [ userExpenses, setUserExpenses ] = useState([]);
   const [ openForm, setOpenForm ] = useState(false);
-  const api = useAxios();
+  const apiRef = useRef(useAxios());
+
+  const loadData = async () => {
+  
+    try {
+      const expenses = await apiRef.current.get('/api/user_expenses', {
+        params: {
+          fromDate: '15-02-2023',
+          toDate: '20-03-2023'
+        }
+      });
+      setUserExpenses(expenses.data);
+    } catch (e) {
+      Swal.fire({
+        title: 'Failed to load transactions',
+        text: e.message,
+        icon: 'error',
+        confirmButtonText: 'Close',
+      })
+    }
+  }
 
   useEffect(() => {
-    const loadData = async () => {
-      try{
-        const expenses = await api.get('/api/user_expenses', {
-          params: {
-            fromDate: '15-02-2023',
-            toDate: '20-03-2023'
-          }
-        });
-        console.log(expenses)
-        setUserExpenses(expenses.data);
-      } catch (error) {
-        console.log('Error:', error.message);
-      }
-    }
-
     loadData();
-  }, [])
+  }, []);
 
   return (
     <div className='pageWrapper'>
-      <TransactionsForm isOpen={openForm} setIsOpen={setOpenForm} />
+      <TransactionsForm isOpen={openForm} setIsOpen={setOpenForm} loadTransactions={loadData} />
       <h1>Transactions Overview</h1>
       <div className='transactionsWrapper'>
         <div className='transactionsHeader'>
@@ -64,9 +72,7 @@ const TransactionsOverviewPage = () => {
           }
         </div>
         <div className='transactionsFooter'>
-          <div className='primaryLink' onClick={() => setOpenForm(true)}>
-            Add expense
-          </div>
+          <FontAwesomeIcon onClick={() => setOpenForm(true)} icon={faCirclePlus} size='3x' color='green'/>
         </div>
       </div>
     </div>
