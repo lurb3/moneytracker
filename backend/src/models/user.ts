8674112c4149ee017b93fb0a2f1980531bd1b5ef
gustavo <gustavo.gigante.s@gmail.com>
2023-04-15 00:38:40 +0100

    fix issue with invalid user password

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -10,7 +10,7 @@ const defaultCategories = ['Technology', 'Health & well being', 'Home bills', 'F
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, immutable: true },
   totalBudget: { type: Number, default: 0 },
   categories: { type: Array, default: defaultCategories },
   token: { type: String },
