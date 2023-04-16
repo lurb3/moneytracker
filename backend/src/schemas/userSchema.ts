619d75eae74f42cd69fde8ccff894e0e7d6a4f8a
gustavo <gustavo.gigante.s@gmail.com>
2023-04-16 18:16:48 +0100

    Fix signup issue

--- backend/src/schemas/userSchema.ts
+++ backend/src/schemas/userSchema.ts
@@ -1,9 +1,11 @@
-import Joi from 'joi'
+import Joi from 'joi';
 
 const userSchema = Joi.object({
   username: Joi.string().max(255),
   email: Joi.string().email(),
-  password: Joi.string()
+  password: Joi.string(),
+  categories: Joi.array(),
+  totalBudget: Joi.number(),
 });
 
 export default userSchema;
